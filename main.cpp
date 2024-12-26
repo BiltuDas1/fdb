dbe6710d7c65c2b4d2a1ed336181fd009ebf3b02
BiltuDas1 <billionto@gmail.com>
2024-12-26 13:58:47 +0530

    Changed in network layer so that when fdb terminated, resource will be cleared

--- main.cpp
+++ main.cpp
@@ -6,11 +6,29 @@
 
 using std::cout, std::cerr, std::endl, std::string;
 
+uv_tcp_t server;
+uv_loop_t *loop;
+
+
+void on_signal(uv_signal_t *req, int signum) {
+  cout << "\nTurning off the server... ";
+
+  // Close the server handle
+  uv_close((uv_handle_t *)&server, [](uv_handle_t *handle) {
+      cout << "Done" << endl;
+  });
+
+  // Stop the event loop
+  uv_stop(loop);
+  uv_signal_stop(req);
+  uv_close((uv_handle_t *)req, nullptr);
+}
+
+
 int main() {
   char *port = std::getenv("PORT");
 
-  uv_loop_t *loop = uv_default_loop();
-  uv_tcp_t server;
+  loop = uv_default_loop();
   uv_tcp_init(loop, &server); // Initialize the TCP Server
 
   sockaddr_in addr;
@@ -34,6 +52,11 @@ int main() {
   } else {
     cout << "Database is running on port " << port << endl;
   }
+
+  // Signal handling for graceful shutdown
+  uv_signal_t sig;
+  uv_signal_init(loop, &sig);
+  uv_signal_start(&sig, on_signal, SIGINT); // Handle Ctrl+C (SIGINT)
 
   // Start the server along with Infinite loop
   uv_run(loop, UV_RUN_DEFAULT);
